30a1444e926863626772db76e2c9a61144bd8d8e
Sagnik Kundu <sagnikk@vmware.com>
2016-01-07 16:55:41 +0530

    vrops bash

--- single_node_vrops/manual_deploy.sh
+++ single_node_vrops/manual_deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# Author: William Lam
-# Website: www.virtuallyghetto.com
 # Product: VMware vRealize Operations Manager 6.0
 # Description: Wrapper script to deploy vRealize Operations Manager 6.0 (vROps)
-# Reference: http://www.virtuallyghetto.com/2014/12/automate-deployment-configuration-of-vrealize-operations-manager-6-0-part-1.html
 
 # Configurations 
 
